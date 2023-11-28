b158c348cff95551d9c28769520ded46bebb9e5d
Palldanerar <alexkharin86@gmail.com>
2023-11-28 13:59:51 +0300

    Setup

---
+++ app/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full h-auto py-3 flex items-center justify-center bg-slate-800'>
+        <h2 className='text-white text-2xl'>Color-Get</h2>
+    </header>
+  )
+}
+
+export default Header
